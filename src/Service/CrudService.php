50cb60d26581f06ec2436644b0e5eee078d255fc
umitkarasogut <umtkrsgt@hotmail.com>
2022-08-23 10:38:47 +0300

    [refactor] code refactor

--- src/Service/CrudService.php
+++ src/Service/CrudService.php
@@ -3,38 +3,39 @@
 namespace Drupal\crud_module\Service;
 
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
-use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Entity\{EntityTypeManager, EntityStorageBase};
+
 
 class CrudService extends ServiceProviderBase {
 
-  private $entityTypeManager;
+    private EntityStorageBase $entityTypeManager;
 
-  public function __construct(EntityTypeManager $entityTypeManager) {
-    $this->entityTypeManager = $entityTypeManager->getStorage('product');
-  }
-
-  public function find($id) {
-    return $this->entityTypeManager->load($id);
-  }
-
-  public function findAll() {
-    return $this->entityTypeManager->loadMultiple();
-  }
-
-  public function create($data) {
-    return $this->entityTypeManager->create($data)->save();
-  }
-
-  public function update($id, $data) {
-    $product = $this->find($id);
-    foreach ($data as $key => $value) {
-      $product->set($key, $value);
+    public function __construct(EntityTypeManager $entityTypeManager) {
+        $this->entityTypeManager = $entityTypeManager->getStorage('product');
     }
-    $product->save();
-  }
 
-  public function delete($id) {
-    $this->entityTypeManager->load($id)->delete();
-  }
+    public function find($id) {
+        return $this->entityTypeManager->load($id);
+    }
+
+    public function findAll() {
+        return $this->entityTypeManager->loadMultiple();
+    }
+
+    public function create($data) {
+        return $this->entityTypeManager->create($data)->save();
+    }
+
+    public function update($id, $data) {
+        $product = $this->find($id);
+        foreach ($data as $key => $value) {
+            $product->set($key, $value);
+        }
+        $product->save();
+    }
+
+    public function delete($id) {
+        $this->entityTypeManager->load($id)->delete();
+    }
 
 }
--- tests/src/Kernel/CrudServiceTest.php
+++ tests/src/Kernel/CrudServiceTest.php
@@ -2,61 +2,60 @@
 
 namespace Drupal\crud_module\tests\Kernel;
 
+use Drupal\KernelTests\KernelTestBase;
 use Drupal\crud_module\{Entity\Product, Service\CrudService};
-use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
 
-class CrudServiceTest extends EntityKernelTestBase {
+class CrudServiceTest extends KernelTestBase {
 
-  protected static $modules = ['crud_module'];
+    protected static $modules = ['crud_module'];
 
-  protected int $exampleProductId;
+    protected int $exampleProductId;
 
-  protected array $exampleProduct = [
-    'name' => 'iphone',
-    'price' => 10,
-  ];
+    protected array $exampleProduct = [
+        'name' => 'iphone',
+        'price' => 10,
+    ];
 
-  protected function setUp(): void {
-    parent::setUp();
-    $this->installConfig(['crud_module']);
-    $this->installEntitySchema('product');
+    protected function setUp(): void {
+        parent::setUp();
+        $this->installEntitySchema('product');
+        $entityTypeManager = $this->container->get('entity_type.manager');
+        $this->service = new CrudService($entityTypeManager);
+        $this->exampleProductId = $this->service->create($this->exampleProduct);
+    }
 
-    $this->service = new CrudService($this->entityTypeManager);
-    $this->exampleProductId = $this->service->create($this->exampleProduct);
-  }
+    public function test_user_can_find_product() {
+        $product = $this->service->find($this->exampleProductId);
 
-  public function test_user_can_find_product() {
-    $product = $this->service->find($this->exampleProductId);
+        $this->assertInstanceOf(Product::class, $product);
+        $this->assertEquals($this->exampleProduct['name'], $product->get('name')->value);
+        $this->assertEquals($this->exampleProduct['price'], $product->get('price')->value);
+        //$this->assertEquals($this->exampleProduct, $product->toArray());
+    }
 
-    $this->assertInstanceOf(Product::class, $product);
-    $this->assertEquals($this->exampleProduct['name'], $product->get('name')->value);
-    $this->assertEquals($this->exampleProduct['price'], $product->get('price')->value);
-    //$this->assertEquals($this->exampleProduct, $product->toArray());
-  }
+    public function test_user_can_list_product() {
+        $products = $this->service->findAll();
+        $this->assertIsArray($products);
 
-  public function test_user_can_list_product() {
-    $products = $this->service->findAll();
-    $this->assertIsArray($products);
+        $product = reset($products);  # or $product = $products[0];
+        $this->assertInstanceOf(Product::class, $product);
+        $this->assertEquals($this->exampleProduct['name'], $product->get('name')->value);
+        $this->assertEquals($this->exampleProduct['price'], $product->get('price')->value);
+    }
 
-    $product = reset($products);  # or $product = $products[0];
-    $this->assertInstanceOf(Product::class, $product);
-    $this->assertEquals($this->exampleProduct['name'], $product->get('name')->value);
-    $this->assertEquals($this->exampleProduct['price'], $product->get('price')->value);
-  }
+    public function test_user_can_update_product() {
+        $newProductData = ['name' => 'Updated Name !', 'price' => 10000];
+        $this->service->update($this->exampleProductId, $newProductData);
 
-  public function test_user_can_update_product() {
-    $newProductData = ['name' => 'Updated Name !', 'price' => 10000];
-    $this->service->update($this->exampleProductId, $newProductData);
+        $product = $this->service->find($this->exampleProductId);
+        $this->assertEquals($newProductData['name'], $product->get('name')->value);
+        $this->assertEquals($newProductData['price'], $product->get('price')->value);
+    }
 
-    $product = $this->service->find($this->exampleProductId);
-    $this->assertEquals($newProductData['name'], $product->get('name')->value);
-    $this->assertEquals($newProductData['price'], $product->get('price')->value);
-  }
-
-  public function test_user_can_delete_product() {
-    $this->service->delete($this->exampleProductId);
-    $product = $this->service->find($this->exampleProductId);
-    $this->assertEquals(NULL, $product);
-  }
+    public function test_user_can_delete_product() {
+        $this->service->delete($this->exampleProductId);
+        $product = $this->service->find($this->exampleProductId);
+        $this->assertEquals(NULL, $product);
+    }
 
 }
